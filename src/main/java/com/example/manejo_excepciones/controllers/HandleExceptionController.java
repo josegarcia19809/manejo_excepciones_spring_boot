ece388eb3f41e099c514e59a7e2e23798ac469b9
josegarcia19809 <josegarcia19809@gmail.com>
2025-01-19 07:41:34 -0600

    Manejo de excepciones: Añadiendo método handler para el manejo de excepción

--- src/main/java/com/example/manejo_excepciones/controllers/HandleExceptionController.java
+++ src/main/java/com/example/manejo_excepciones/controllers/HandleExceptionController.java
@@ -1,14 +1,24 @@
 package com.example.manejo_excepciones.controllers;
 
+import com.example.manejo_excepciones.models.Error;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Date;
 
 @RestControllerAdvice
 public class HandleExceptionController {
 
     @ExceptionHandler({ArithmeticException.class})
-    public ResponseEntity<?> divisionByZero(Exception ex) {
-        return ResponseEntity.internalServerError().body("Error al dividir entre cero");
+    public ResponseEntity<Error> divisionByZero(Exception ex) {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Error al dividir entre cero");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        // return ResponseEntity.internalServerError().body(error);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 }
